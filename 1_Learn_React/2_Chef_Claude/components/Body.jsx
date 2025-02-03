784b5948ee2b59d7326ce9fd252d70ed2320df3b
Deren B <dbozerprogramming@gmail.com>
2025-02-02 20:19:39 -0500

    Adding an ingredient List

--- 1_Learn_React/2_Chef_Claude/components/Body.jsx
+++ 1_Learn_React/2_Chef_Claude/components/Body.jsx
@@ -1,7 +1,36 @@
 export default function Body() {
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log("Form submitted")
+
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+
+        ingredients.push(newIngredient)
+
+        console.log(ingredients)
+    }
+
     return (
-        <main>
-            
+        <main className="body">
+            <form onSubmit={handleSubmit} className="add_ingredient_form">
+                <input 
+                    type="text"
+                    aria-label="Add Ingredient" 
+                    placeholder="e.g. oregano"
+                    name="ingredient"
+                />
+                <button>Add Ingredient</button>
+            </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
